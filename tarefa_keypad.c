c361f7befd3b74bd38e94d3dd42f43a4974fd04c
Werliarlinson <riugen1@hotmail.com>
2025-01-19 18:30:59 -0300

    Concerta buzzer

--- tarefa_keypad.c
+++ tarefa_keypad.c
@@ -25,6 +25,7 @@ const uint rgb_3[3] = {19, 18, 17};
 const uint buzzer_pin = 14; // GPIO do buzzer
 
 
+
 // Matrize de mapeamento de teclas
 const char keys[ROWS][COLS] = {
     {'1', '2', '3', 'A'},
@@ -35,8 +36,8 @@ const char keys[ROWS][COLS] = {
 
 //função para inicializar o buzzer
 void pico_buzzer_init(uint gpio) {
-    gpio_init(gpio);
-    gpio_set_dir(gpio, GPIO_OUT);
+    //gpio_init(gpio);
+    //gpio_set_dir(gpio, GPIO_OUT);
     gpio_set_function(gpio, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(gpio);
     pwm_set_enabled(slice_num, true);
@@ -45,14 +46,36 @@ void pico_buzzer_init(uint gpio) {
 //função para tocar uma nota no buzzer
 void pico_buzzer_play(uint gpio, uint frequency) {
     uint slice_num = pwm_gpio_to_slice_num(gpio);
-    pwm_set_wrap(slice_num, clock_get_hz(clk_sys) / frequency - 1);
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, (clock_get_hz(clk_sys) / frequency) / 2);
+    //uint32_t clock = clock_get_hz(clk_sys);
+    //uint32_t wrap = clock / frequency - 1;
+    uint32_t clock = 125000000; 
+    uint32_t divider = clock / (frequency * 4096); 
+    uint32_t wrap = (clock / divider) / frequency - 1;
+    uint32_t level = wrap / 2; 
+    //pwm_set_wrap(slice_num, clock_get_hz(clk_sys) / frequency - 1);
+    //pwm_set_chan_level(slice_num, PWM_CHAN_A, (clock_get_hz(clk_sys) / frequency) / 2);
+    pwm_set_clkdiv(slice_num, divider);
+    pwm_set_wrap(slice_num, wrap);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, level);
+    pwm_set_enabled(slice_num, true);
+    /*uint32_t period_us = 1000000 / frequency; // Período em microssegundos
+    uint32_t half_period_us = period_us / 2; // Meio período para alternar o estado do pino
+    uint32_t end_time = to_ms_since_boot(get_absolute_time()) + 250;
+
+    while (to_ms_since_boot(get_absolute_time()) < end_time) {
+        gpio_put(gpio, 1);
+        sleep_us(half_period_us);
+        gpio_put(gpio, 0);
+        sleep_us(half_period_us);
+    }*/
 }
 
 //função para parar o buzzer
 void pico_buzzer_stop(uint gpio) {
     uint slice_num = pwm_gpio_to_slice_num(gpio);
     pwm_set_chan_level(slice_num, PWM_CHAN_A, 0);
+    pwm_set_enabled(slice_num, false);
+    //gpio_put(gpio, 0);
 }
 
 //função para tocar a melodia "Marcha Soldado"
@@ -81,8 +104,7 @@ void play_marcha_soldado(uint gpio) {
 
 //função que inicializa os pinos de um led RGB
 void pico_rgb_init(const uint *rgb_pin)
-{   
-    
+{    
     for(int i = 0; i < 3; i++)
     {
         //_rgb_1[i] = rgb_pin[i];
@@ -97,14 +119,6 @@ void pico_rgb_turn_off(const uint *rgb_pin)
     for(int i = 0; i < 3; i++)
     {
         gpio_put(rgb_pin[i], false);        //garante que todos serão desligados por padrão
-    }
-}
-
-void pico_rgb_turn_on(const uint *rgb_pin)
-{
-    for(int i = 0; i < 3; i++)
-    {
-        gpio_put(rgb_pin[i], true);        //garante que todos serão desligados por padrão
     }
 }
 
@@ -143,22 +157,61 @@ char pico_scan_keypad() {
 void pico_keypad_control_led(char key) {
     switch (key) {
         case '1':
-            gpio_put(rgb_1[0], !gpio_get(rgb_1[0]));    //se a tecla 1 for pressionada, alterna o estado do canal vermelho do led 1
+            gpio_put(rgb_1[0], !gpio_get(rgb_1[0])); // Alterna o estado do canal vermelho do LED 1
             printf("Canal vermelho do led 1 alternado.\n");
             break;
         case '2':
-            gpio_put(rgb_1[1], !gpio_get(rgb_1[1]));
-            printf("Canal verde do led 1 alternado.\n"); //se a tecla 2 for pressionada, alterna o estado do canal verde do led 1
+            gpio_put(rgb_1[1], !gpio_get(rgb_1[1])); // Alterna o estado do canal verde do LED 1
+            printf("Canal verde do led 1 alternado.\n");
             break;
         case '3':
-            gpio_put(rgb_1[2], !gpio_get(rgb_1[2]));    //se a tecla 3 for pressionada, alterna o estado do canal azul do led 1
+            gpio_put(rgb_1[2], !gpio_get(rgb_1[2])); // Alterna o estado do canal azul do LED 1
             printf("Canal azul do led 1 alternado.\n");
             break;
         case 'A':
-            pico_rgb_turn_off(rgb_1); //se a tecla pressionada for o A, desliga todo o led rgb
+            pico_rgb_turn_off(rgb_1); // Desliga o LED 1
             printf("Led 1 desligado\n");
             break;
+        case '4':
+            gpio_put(rgb_2[0], !gpio_get(rgb_2[0])); // Alterna o estado do canal vermelho do LED 2
+            printf("Canal vermelho do led 2 alternado.\n");
+            break;
+        case '5':
+            gpio_put(rgb_2[1], !gpio_get(rgb_2[1])); // Alterna o estado do canal verde do LED 2
+            printf("Canal verde do led 2 alternado.\n");
+            break;
+        case '6':
+            gpio_put(rgb_2[2], !gpio_get(rgb_2[2])); // Alterna o estado do canal azul do LED 2
+            printf("Canal azul do led 2 alternado.\n");
+            break;
+        case 'B':
+            pico_rgb_turn_off(rgb_2); // Desliga o LED 2
+            printf("Led 2 desligado\n");
+            break;
+        case '7':
+            gpio_put(rgb_3[0], !gpio_get(rgb_3[0])); // Alterna o estado do canal vermelho do LED 3
+            printf("Canal vermelho do led 3 alternado.\n");
+            break;
+        case '8':
+            gpio_put(rgb_3[1], !gpio_get(rgb_3[1])); // Alterna o estado do canal verde do LED 3
+            printf("Canal verde do led 3 alternado.\n");
+            break;
+        case '9':
+            gpio_put(rgb_3[2], !gpio_get(rgb_3[2])); // Alterna o estado do canal azul do LED 3
+            printf("Canal azul do led 3 alternado.\n");
+            break;
+        case 'C':
+            pico_rgb_turn_off(rgb_3); // Desliga o LED 3
+            printf("Led 3 desligado\n");
+            break;
+        case '0': // Desliga todos os LEDS de uma vez
+            pico_rgb_turn_off(rgb_1);
+            pico_rgb_turn_off(rgb_2);
+            pico_rgb_turn_off(rgb_3);
+            printf("Todos os LEDS desligados");
+            break;
         default:
+            printf("Tecla '%c' não mapeada.\n", key);
             break;
     }
 }
@@ -206,7 +259,7 @@ void pico_keypad_control_music(char key) {
             sleep_ms(200);
             pico_buzzer_stop(buzzer_pin);
             break;
-        case 'A': play_marcha_soldado(buzzer_pin); break; // Toca a melodia "Marcha Soldado" quando a tecla 'D' é pressionada
+        case 'A': play_marcha_soldado(buzzer_pin); break; // Toca a melodia "Marcha Soldado" quando a tecla 'A' é pressionada
         case '0': pico_buzzer_stop(buzzer_pin); break; // Para de tocar a nota no buzzer
         default: pico_buzzer_stop(buzzer_pin); break;
     }
@@ -220,7 +273,7 @@ int main() {
     pico_setup_keypad();
     pico_rgb_init(rgb_1);
     pico_rgb_init(rgb_2);
-   // pico_rgb_init(rgb_3);
+    pico_rgb_init(rgb_3);
     pico_buzzer_init(buzzer_pin);
     
     while (true) {
@@ -228,7 +281,10 @@ int main() {
         if (key != '\0') {
             if (key == 'D') {
                 music_mode = true; // Muda para o modo de reprodução de música
-                pico_buzzer_play(buzzer_pin, NOTE_DO1); // Toca uma nota para indicar a mudança de modo
+                pico_buzzer_play(buzzer_pin, NOTE_RE); // Toca uma nota para indicar a mudança de modo
+                pico_rgb_turn_off(rgb_1); // Desliga todos os LEDS ao mudar para o modo de música
+                pico_rgb_turn_off(rgb_2);
+                pico_rgb_turn_off(rgb_3);
             } else if (key == '0') {
                 music_mode = false; // Volta para o modo padrão (LEDs)
             }
@@ -238,9 +294,10 @@ int main() {
             } else {
                 pico_keypad_control_led(key); // Executa a ação correspondente no modo padrão (LEDs)
             }
+        }
         sleep_ms(100); // Pausa para evitar leitura repetida contínua
+        
     }
-
     return 0;
-
+    
 }
